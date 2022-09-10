7a2dcf3d56377306be4b178f4b356101d85b151a
nerdfiles <nerdfiles@gmail.com>
2022-09-10 06:35:29 -0500

    reorganized services

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.scss'
 import { useState } from 'react'
-import Logger from './infrastructure/utils/Logger'
-import UserApi from './app/user'
 import WPHeader from './interfaces/WPHeader'
 import WPFooter from './interfaces/WPFooter'
 import Button from './interface.parts/Button'
 import Person from './domain/person'
+import services from './infrastructure/services'
 
 const person = new Person({
   name: 'john',
@@ -18,7 +17,6 @@ const person = new Person({
  * @description
  */
 function App () {
-  const userApi = new UserApi()
   const [clientGetData, clientGetSetData] = useState([])
   const [clientPostData, clientPostSetData] = useState({})
   const [clientDeleteData, clientDeleteSetData] = useState('')
@@ -26,55 +24,6 @@ function App () {
     <p>empty</p>
   )
   const DATA_LIST = clientGetData && clientGetData.length
-
-  /**
-   * clientPostData.
-   * @todo pass object into clientPostData since it's after componentDidMount or whatever
-   * @note postData initializes with '' according to line 9 but after the DOM update from
-   * onInput, it has not been updated
-   */
-  const postData = (_name) => {
-    const NAME = _name
-    const payload = {
-      name: NAME,
-      job: 'surfer'
-    }
-    const payment = userApi.POST(payload)
-    payment
-      .then((res) => {
-        const dataList = res.data
-        Logger({ label: 'res', msg: [dataList] })
-        clientGetSetData([dataList])
-      })
-      .catch(Logger)
-  }
-
-  /**
-   * getData.
-   */
-  const getData = () => {
-    const payment = userApi.GET()
-    payment
-      .then((res) => {
-        const dataList = res.data.data
-        clientGetSetData(dataList)
-      })
-      .catch(Logger)
-  }
-
-  /**
-   * deleteData.
-   */
-  const deleteData = () => {
-    const payment = userApi.DELETE()
-    payment
-      .then((res) => {
-        const dataList = res.data.data
-        clientDeleteSetData(dataList)
-      })
-      .catch(Logger)
-  }
-
   const List = () => {
     return (
       DATA_LIST
@@ -82,7 +31,7 @@ function App () {
           const NAME = dataRef.first_name || dataRef.name
           return (
             <li
-              className=''
+              className='m-view--listing__default__'
               key={keyRef}
             >
               {NAME}
@@ -104,18 +53,18 @@ function App () {
 
       <main className='S-view--loci__default__'>
         <input
-          id=''
-          name=''
+          id='datapoint-entry'
+          name='datapoint-entry'
           onInput={(e) => clientPostSetData(e.currentTarget.value)}
-          className='m-view--datapoint__default__'
+          className='m-view--datapoint-entry__default__'
         />
-        <ul className=''>
-          {List()}
+        <ul className='m-view--datapoint__default__'>
+          <List />
         </ul>
       </main>
 
       <nav
-        className='S-view--websitenavigation__default__'
+        className='S-view--website-navigation__default__'
         itemScope
         itemType='https://schema.org/SiteNavigationElement'
       >
@@ -124,20 +73,20 @@ function App () {
             <Button
               textLabel='get'
               concept={person}
-              act={() => getData()}
+              act={() => services.getData(clientGetSetData)}
             />
           </li>
           <li className='m-view--listing__default__'>
             <Button
               textLabel='post'
               concept={person}
-              act={() => postData(clientPostData)}
+              act={() => services.postData(clientPostData, clientPostSetData)}
             />
           </li>
           <li className='m-view--listing__default__'>
             <Button
               textLabel='delete'
-              act={() => deleteData(clientDeleteData)}
+              act={() => services.deleteData(clientDeleteData, clientDeleteSetData)}
             />
           </li>
         </ul>
---
+++ src/infrastructure/services/index.js
@@ -0,0 +1,63 @@
+import UserApi from '../../app/user'
+import Logger from '../../infrastructure/utils/Logger'
+
+const userApi = new UserApi()
+
+/**
+ * @name clientPostData
+ * @todo pass object into clientPostData since it's after componentDidMount or whatever
+ * @note postData initializes with '' according to line 9 but after the DOM update from
+ * onInput, it has not been updated
+ */
+const postData = (_name, callback) => {
+  const NAME = _name
+  const payload = {
+    name: NAME,
+    job: 'surfer'
+  }
+  const payment = userApi.POST(payload)
+  payment
+    .then((res) => {
+      const dataList = res.data
+      Logger({ label: 'res', msg: [dataList] })
+      const outcome = [dataList]
+      callback(outcome)
+    })
+    .catch(Logger)
+}
+
+/**
+ * getData.
+ */
+const getData = (callback) => {
+  const payment = userApi.GET()
+  payment
+    .then((res) => {
+      const dataList = res.data.data
+      callback(dataList)
+    })
+    .catch(Logger)
+}
+
+/**
+ * deleteData.
+ */
+const deleteData = (callback) => {
+  const payment = userApi.DELETE()
+  payment
+    .then((res) => {
+      const dataList = res.data.data
+      callback(dataList)
+    })
+    .catch(Logger)
+}
+
+const services = {
+  getData: getData,
+  postData: postData,
+  deleteData: deleteData
+}
+
+export default services
+
+// EOF
