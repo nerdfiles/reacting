e72cf63f43bd40335f08c44a57c21205563f3fda
nerdfiles <nerdfiles@gmail.com>
2022-09-12 15:48:00 -0500

    clone up

---
+++ reacting/infrastructure/queue/index.js
@@ -0,0 +1,16 @@
+/**
+ * @name Queue
+ * @author aha <patagnome@protonmail.com>
+ * @version 0.0.0-novel.0
+ * @since 0.0.0-novel.0
+ * @kind
+ * # queues
+ *
+ * 1. install redis as a separate app in heroku
+ * 2. or look for rabbitmq elsewhere
+ */
+class Queue {
+
+}
+
+export default Queue
--- reacting/infrastructure/queues/index.js
+++ reacting/infrastructure/queues/index.js
@@ -1,23 +1,23 @@
 /**
  * @filepath ./src/infrastructure/queues/index.js
  * @fileoverview
- * @name Nonce
+ * @name Queue
  * @classdesc
  * @author aha <patagnome@protonmail.com>
  * @license wtfpl, version 2
  * @description
  * # queues
- * @kind Nonce
+ * @kind Queue
  * @version 0.0.0-novel.0
  * @since 0.0.0-novel.0
  * @todo
  * 1. install redis as a separate app in heroku
  * 2. or look for rabbitmq elsewhere
  */
-class Nonce {
+class Queue {
 
 }
 
-export default Nonce
+export default Queue
 
 // EOF
