93194d6fd37061b4cd6994e0e671475b2eb4a044
nerdfiles <nerdfiles@gmail.com>
2022-09-13 01:39:35 -0500

    last

--- src/app.freedom/Unification/index.js
+++ src/app.freedom/Unification/index.js
@@ -1,5 +1,11 @@
 /**
- * @filepath ./src/app.freedom/Unification
+ * @filepath ./src/app.freedom/Unification/index.js
+ * @fileoverview a unification document for mereological-heterogeneity and 
+ * categorical/ontological homogeneity complexity analysis.
+ * @name Unification
+ * @author aha <patagnome@protonmail.com>
+ * @version 0.0.0-novel.0
+ * @since 0.0.0-novel.0
  * @description a unified field of subjectivization of the sentence-subjects.
  */
 class Unification {
