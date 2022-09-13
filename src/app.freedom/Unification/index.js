701e41b7bf3239a335db5186ab02967398e4f178
nerdfiles <nerdfiles@gmail.com>
2022-09-13 01:33:36 -0500

    w/e

---
+++ src/app.freedom/Unification/index.js
@@ -0,0 +1,10 @@
+/**
+ * @filepath ./src/app.freedom/Unification
+ * @description a unified field of subjectivization of the sentence-subjects.
+ */
+class Unification {
+  theory () {}
+  praxis () {}
+}
+
+export default Unification
