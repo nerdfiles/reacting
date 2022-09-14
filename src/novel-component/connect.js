9ea6084ce80a759010bcd12f4234d3cb145bd930
nerdfiles <nerdfiles@gmail.com>
2022-09-14 09:47:18 -0500

    docs update

--- src/novel-component/connect.js
+++ src/novel-component/connect.js
@@ -8,6 +8,9 @@ import { GET_NAME } from './actions'
 import NovelComponent from './index'
 
 export default connect((occasion) => {
+  // @description state management is only one control plane among activity 
+  // history, cache, echoes, forms, chords, ratings, timers, heartbeats, etc.
+  // @see https://apisyouwonthate.com/blog/common-hypermedia-patterns-with-json-hyper-schema
   if (occasion && occasion['next']) {
     return {
       key: occasion && occasion.next
