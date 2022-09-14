172aedad3063d71cb56414e14b18c61173d4fe14
nerdfiles <nerdfiles@gmail.com>
2022-09-14 09:55:01 -0500

    docs update

--- src/novel-component/connect.js
+++ src/novel-component/connect.js
@@ -8,9 +8,15 @@ import { GET_NAME } from './actions'
 import NovelComponent from './index'
 
 export default connect((occasion) => {
-  // @description state management is only one control plane among activity 
-  // history, cache, echoes, forms, chords, ratings, timers, heartbeats, etc.
+  // @description state management is only one control plane among activity
+  // history, cache, echoes, `<form>`s, chords, ratings, timers, heartbeats, etc.
+  //
+  // i consider metamodeling hypermedia web using procedural hypertext
+  // generation with abstractive logic alphabet. [fazi] presents a distinction
+  // between the abstractive and abstractness which reflect rendencies in
+  // specialization checking specificity development over concretization.
   // @see https://apisyouwonthate.com/blog/common-hypermedia-patterns-with-json-hyper-schema
+
   if (occasion && occasion['next']) {
     return {
       key: occasion && occasion.next
