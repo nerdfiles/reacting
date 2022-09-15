cdde0c3d3ea32887c0568f00ddef4e6cf4a0a300
nerdfiles <nerdfiles@gmail.com>
2022-09-15 10:47:00 -0500

    docs update

--- src/interface.parts/Link.js
+++ src/interface.parts/Link.js
@@ -1,5 +1,19 @@
 /**
+ * @filepath ./src/interface.parts/Link.js
+ * @fileoverview _.
+ * @author aha <patagnome@protonmail.com>
+ * @license wtfpl, version 2
+ * @since 0.0.0-novel.0
+ * @version 0.0.0-novel.0
+ * @kind _.
+ * @todo none
  * @name Link
+ * @function
+ * @see web.dev/apply-instant-loading-with-prpl/
+ * @see web.dev/rail/
+ * @description _.
+ * @todo browse _.
+ * @returns {HTMLLinkElement} markup.
  */
 const Link = (props) => {
   return (
