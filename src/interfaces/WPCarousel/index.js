a05bd2d8174e6697dcb9b17c0a4a9f490eaeed44
nerdfiles <nerdfiles@gmail.com>
2022-09-16 19:37:41 -0500

    docs update

--- src/interfaces/WPCarousel/index.js
+++ src/interfaces/WPCarousel/index.js
@@ -7,40 +7,104 @@ import WPRolodex from './WPRolodex'
 const WPCarousel = () => {
 
   return (
-    <section>
+    <section className='O-view--carousel__default__'>
 
-      <header>
-        <h2>wpcarousel</h2>
+      <header className='o-view--title-line__default__'>
+        <h2 className='m-view--carousel-title'>wpcarousel</h2>
       </header>
 
-      <main>
-        <ul>
-          <li>
-            <WPArticle>article</WPArticle>
+      <main className='o-view--carousel__default__'>
+        <ul className='m-view--carousel-list__default__'>
+          <li className='m-view--listing__default__'>
+            <WPArticle>
+              <h3 className='a-view--title__default__'>title</h3>
+              <figure className='a-view-detail__default__'>
+                <img
+                  className='a-view-image__default__'
+                  alt='title'
+                  src='./assets/content-images/0.png'
+                />
+                <figcaption className='a-view-caption__default__'>
+                </figcaption>
+              </figure>
+            </WPArticle>
           </li>
         </ul>
       </main>
 
       <WPRolodex />
 
-      <nav>
-        <ul>
-          <li class='bullet'>article</li>
-        </ul>
-      </nav>
+      {
+        /**
+         * @example WPRolodex
+         * <nav>
+         *   <ul>
+         *     <li class='bullet'>article</li>
+         *   </ul>
+         * </nav>
+         */
+      }
 
-      <footer>
-        <ul>
-          <li>
-            <h3>&lt;rel='related'&gt;</h3>
-            <ul>
-              <li>...</li>
+      <footer className='o-view--carousel__default__'>
+        <ul className='m-view--carousel-list__default__'>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='related'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
             </ul>
           </li>
-          <li>
-            <h3>&lt;rel='used'&gt;</h3>
-            <ul>
-              <li>...</li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='used'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='watched'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='submitted'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='navigation'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='fixtures'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='profiles'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='ignores'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='forwards'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
+            </ul>
+          </li>
+          <li className='m-view--listing__default__'>
+            <h3 className='m-view-title__default__'>&lt;rel='queue'&gt;</h3>
+            <ul className='m-view--list__default__'>
+              <li className='m-view--listing__default__'>...</li>
             </ul>
           </li>
         </ul>
